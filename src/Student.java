7a910e795194903c1a88b750c62e28913795765e
evgenii_trzhtsetskii <tevgen@gmail.com>
2021-06-02 15:39:35 +0300

    initial commit

---
+++ src/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+
+    int numberStudentBilet;
+    String studentName;
+    String studentFamaly;
+    int earsLesson;
+    String age;
+    double ratingMatematica;
+    double ratingEconomica;
+    double ratingEnglish;
+    String items;
+
+    public Student (String studentName, String studentFamaly, int numberStudentBilet, double ratingMatematica, double ratingEconomica, double ratingEnglish, int earsLesson){
+
+        this.studentName = studentName;
+        this.studentFamaly = studentFamaly;
+        this.numberStudentBilet = numberStudentBilet;
+        this.ratingMatematica = ratingMatematica;
+        this.ratingEconomica = ratingEconomica;
+        this.ratingEnglish = ratingEnglish;
+        this.earsLesson = earsLesson;
+
+    }
+
+    public Student (){
+
+        }
+
+    double rating (){
+        //System.out.println(rating());
+        return (this.ratingMatematica + this.ratingEnglish + this.ratingEconomica)/3;
+    }
+
+    String age1 () {
+        if (this.earsLesson >= 5) {
+            return this.age = " лет";
+
+        } else if (this.earsLesson <= 4) {
+            return this.age = " года";
+
+        } else
+            return this.age;
+    }
+
+}
---
+++ src/StudentTest.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.nio.*;
+
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+
+//        Student student = new Student();
+//        student.add("Иван", "Федоров,", 568743,5,3,4,5);
+//        student.add("Пётр", "Соколов,", 965782,5,5,5,4);
+//        student.add("Михаил", "Кошелев,", 835941,3,3,3,6);
+//        System.out.println(student.items);
+//
+//        //Запись данных в файл
+//        String data = student.items;
+        
+
+
+        System.out.println("\nПеречень студентов: \n");
+        Student Ivan = new Student("Иван", "Федоров,", 568743,5,3,4,5);
+        System.out.println("Средний рейтинг студента " + (Ivan.rating()));
+        System.out.println("Обучается в университете в годах: " + Ivan.earsLesson + (Ivan.age1()));
+        System.out.println(Ivan.studentName + " " + Ivan.studentFamaly + " Номер студентчиского билета: " + Ivan.numberStudentBilet);
+
+
+
+        Student Petr = new Student("Пётр", "Соколов,", 965782,5,5,5,4);
+        System.out.println("\nСредний рейтинг студента " + (Petr.rating()));
+        System.out.println("Обучается в университете в годах: " + Petr.earsLesson + (Petr.age1()));
+        System.out.println(Petr.studentName + " " + Petr.studentFamaly + " Номер студентчиского билета: " + Petr.numberStudentBilet);
+
+        Student Misha = new Student("Михаил", "Кошелев,", 835941,3,3,3,6);
+        System.out.println("\nСредний рейтинг студента " + (Misha.rating()));
+        System.out.println("Обучается в университете в годах: " + Misha.earsLesson + (Misha.age1()));
+        System.out.println(Misha.studentName + " " + Misha.studentFamaly + " Номер студентчиского билета: " + Misha.numberStudentBilet);
+
+        //writeUsingFileWriter(data);
+
+
+    }
+// Подробности записи тут https://javadevblog.com/kak-zapisat-v-fajl-na-java-primery-s-filewriter-bufferedwriter-files-i-fileoutputstream.html
+
+    private static void writeUsingFileWriter(String data) {
+        File file = new File("FileWriter.txt");
+        FileWriter fr = null;
+        try {
+            fr = new FileWriter(file);
+            fr.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
